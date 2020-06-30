e7ecc890cb3dd058d5bfc56a84faa58f49dfde94
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-06-30 11:00:22 +0930

    Update domain_recon.sh

--- domain_recon.sh
+++ domain_recon.sh
@@ -7,9 +7,12 @@ FILENAME="$TODAY.zip"
 FILENAME_BASE64=$(printf "%s" "$FILENAME" | base64)
 echo "[DEBUG] Filename: $FILENAME_BASE64"
 
-DIR="./"
+DIR="./domains"
+[ -d $DIR ] || mkdir $DIR
 
 URL="https://whoisds.com/whois-database/newly-registered-domains/$FILENAME_BASE64/nrd"
 echo "[DEBUG] Downloading $URL"
 
-wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "$URL" -O domains.zip
+wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "$URL" -O $DIR/$TODAY.zip
+
+unzip $DIR/$TODAY.zip -d $DIR
