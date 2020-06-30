2feb9197ebf29cef009dcad485d4a12361ee61f9
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-06-30 10:49:13 +0930

    Update domain_recon.sh

--- domain_recon.sh
+++ domain_recon.sh
@@ -8,11 +8,12 @@ FILENAME_NO_WHITESPACE="$(echo -e "${FILENAME}" | tr -d '[:space:]')"
 echo "[DEBUG] Filename: $FILENAME_NO_WHITESPACE"
 
 FILENAMEB64=`echo $FILENAME_NO_WHITESPACE | base64`
+FILENAMEB64_NO_WHITESPACE="$(echo -e "${FILENAMEB64}" | tr -d '[:space:]')"
 echo "[DEBUG] FilenameB64: $FILENAMEB64"
 
 DIR="./"
 
-URL="https://whoisds.com/whois-database/newly-registered-domains/$FILENAME/nrd"
+URL="https://whoisds.com/whois-database/newly-registered-domains/$FILENAMEB64_NO_WHITESPACE/nrd"
 echo "[DEBUG] Downloading $URL"
 
 wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "$URL" -O domains.zip
