871aedcaea5ff91fd590bca386ff58dbb1bc3080
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-06-30 10:41:25 +0930

    Update domain_recon.sh

--- domain_recon.sh
+++ domain_recon.sh
@@ -11,25 +11,4 @@ DIR="./"
 URL="https://whoisds.com/whois-database/newly-registered-domains/$FILENAME/nrd"
 echo "[DEBUG] Downloading $URL"
 
-TMP=`mktemp ./XXXXXX.zip`
-LOG=`mktemp ./XXXXXX.log`
-
-[ -d "$DIR" ] || mkdir -p $DIR
-[ -r "$DIR/$TODAY.txt" ] && rm "$DIR/$TODAY.txt"
-
-wget -o $LOG -O domains.zip $URL
-RC=$?
-if [ "$RC" != "0" ]; then
-  echo "Cannot fetch URL"
-  cat $LOG
-else
-  unzip -d $DIR $TMP >$LOG 2>&1
-  RC=$?
-  if [ "$RC" != "0" ]; then
-    echo "Cannot unzip $TMP"
-    cat $LOG
-  else
-    [ -r "$DIR/domain-names.txt" ] && mv "$DIR/domain-names.txt" "$DIR/$TODAY.txt"
-    rm $LOG $TMP
-  fi
-fi
+wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "$URL" -O domains.zip
