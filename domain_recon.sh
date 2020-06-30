da6ef149b0f7fee5a5b8c0f005127e0861bb8b35
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-06-30 10:53:59 +0930

    Update domain_recon.sh

--- domain_recon.sh
+++ domain_recon.sh
@@ -4,16 +4,12 @@ TODAY=`date --date="-2days " +"%Y-%m-%d"`
 echo "[DEBUG] Date: $TODAY"
 
 FILENAME="$TODAY.zip"
-FILENAME_NO_WHITESPACE="$(echo -n "${FILENAME}" | tr -d '[:space:]')"
-echo "[DEBUG] Filename: $FILENAME_NO_WHITESPACE"
-
-FILENAMEB64=`echo $FILENAME_NO_WHITESPACE | base64`
-FILENAMEB64_NO_WHITESPACE="$(echo -n "${FILENAMEB64}" | tr -d '[:space:]')"
-echo "[DEBUG] FilenameB64: $FILENAMEB64"
+FILENAME_BASE64=$(printf "%s" "$FILENAME" | base64)
+echo "[DEBUG] Filename: $FILENAME_BASE64"
 
 DIR="./"
 
-URL="https://whoisds.com/whois-database/newly-registered-domains/$FILENAMEB64_NO_WHITESPACE/nrd"
+URL="https://whoisds.com/whois-database/newly-registered-domains/$FILENAME_BASE64/nrd"
 echo "[DEBUG] Downloading $URL"
 
 wget -U "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "$URL" -O domains.zip
